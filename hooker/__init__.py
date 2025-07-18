c03d05a00d197ceb75b57c64a76b70aba0167d7f
George Muscat <george@muscat.sh>
2025-07-18 17:11:46 +0930

    update default db

--- hooker/__init__.py
+++ hooker/__init__.py
@@ -4,7 +4,7 @@ import psycopg
 PG_HOST = os.getenv("PGHOST", "localhost")
 PG_USER = os.getenv("PGUSER", "postgres")
 PG_PASSWORD = os.getenv("PGPASSWORD", "devpassword")
-PG_DATABASE = os.getenv("PGDATABASE", "stats")
+PG_DATABASE = os.getenv("PGDATABASE", "db")
 PG_PORT = os.getenv("PGPORT", "5432")
 
 DB_URL = f"postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DATABASE}"
