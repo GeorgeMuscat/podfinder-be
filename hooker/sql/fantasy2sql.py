f57f0d059faafa2fff20b07b44baa908600c2cee
George Muscat <george@muscat.sh>
2025-07-18 17:11:37 +0930

    sql insert team

--- hooker/sql/fantasy2sql.py
+++ hooker/sql/fantasy2sql.py
@@ -1,18 +1,25 @@
+import psycopg
 from ..fantasy import get_all_teams
 from hooker import conn
 
 def insert_teams():
-    with conn.cursor() as cur:
-        teams = get_all_teams()
-        for team in teams:
-            cur.execute(
-                """
-                INSERT INTO fantasy_team (external_fantasy_id, full_name, short_name, abbreviation)
-                VALUES (%s, %s, %s, %s)
-                ON CONFLICT (external_fantasy_id) DO UPDATE SET
-                    full_name = EXCLUDED.full_name,
-                    short_name = EXCLUDED.short_name,
-                    abbreviation = EXCLUDED.abbreviation
-                """,
-                (team["id"], team["full_name"], team["name"], team["short_name"]),
-            )
+    try:
+        with conn.cursor() as cur:
+            teams = get_all_teams()
+            for team in teams:
+                cur.execute(
+                    """
+                    INSERT INTO teams (external_fantasy_id, full_name, short_name, abbreviation)
+                    VALUES (%s, %s, %s, %s)
+
+                    """,
+                    (team["id"], team["full_name"], team["name"], team["short_name"]),
+                )
+        conn.commit()
+    except (Exception, psycopg.DatabaseError) as error:
+        print(error)
+
+
+if __name__ == "__main__":
+    insert_teams()
+    print("Teams inserted successfully.")
