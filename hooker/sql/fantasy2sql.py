e529b668fc9ae8aee32322c64b55d594fd4d8be5
George Muscat <george@muscat.sh>
2025-07-18 17:38:54 +0930

    update insert_teams to update on conflicting team

--- hooker/sql/fantasy2sql.py
+++ hooker/sql/fantasy2sql.py
@@ -11,7 +11,7 @@ def insert_teams():
                     """
                     INSERT INTO teams (external_fantasy_id, full_name, short_name, abbreviation)
                     VALUES (%s, %s, %s, %s)
-
+                    ON CONFLICT (external_fantasy_id) DO UPDATE
                     """,
                     (team["id"], team["full_name"], team["name"], team["short_name"]),
                 )
