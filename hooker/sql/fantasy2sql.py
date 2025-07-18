ab37580d82d6750372528fd97a3d6883c35bdbca
George Muscat <george@muscat.sh>
2025-07-18 18:05:00 +0930

    do nothing when duplicate teams

--- hooker/sql/fantasy2sql.py
+++ hooker/sql/fantasy2sql.py
@@ -3,23 +3,20 @@ from ..fantasy import get_all_teams
 from hooker import conn
 
 def insert_teams():
-    try:
-        with conn.cursor() as cur:
-            teams = get_all_teams()
-            for team in teams:
-                cur.execute(
-                    """
-                    INSERT INTO teams (external_fantasy_id, full_name, short_name, abbreviation)
-                    VALUES (%s, %s, %s, %s)
-                    ON CONFLICT (external_fantasy_id) DO UPDATE
-                    """,
-                    (team["id"], team["full_name"], team["name"], team["short_name"]),
-                )
-        conn.commit()
-    except (Exception, psycopg.DatabaseError) as error:
-        print(error)
+    with conn.cursor() as cur:
+        teams = get_all_teams()
+        for team in teams:
+            cur.execute(
+                """
+                INSERT INTO teams (external_fantasy_id, full_name, short_name, abbreviation)
+                VALUES (%s, %s, %s, %s)
+                ON CONFLICT (external_fantasy_id) DO NOTHING;
+                """,
+                (team["id"], team["full_name"], team["name"], team["short_name"])
+            )
+    conn.commit()
+
 
 
 if __name__ == "__main__":
     insert_teams()
-    print("Teams inserted successfully.")
